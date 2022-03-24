3172a70dda2104175c27a3d3f254acfdd74b2a1a
SaltySaff <danielalexander.ash@gmail.com>
2022-03-24 21:01:27 +0000

    Add event creation form

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,27 @@
 class EventsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
   def index
     @events = Event.all
+  end
+
+  def new
+    @event = current_user.created_events.build
+  end
+
+  def create
+    @event = current_user.created_events.build(event_params)
+
+    if @event.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :body)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_24_151827) do
+ActiveRecord::Schema.define(version: 2022_03_24_202343) do
 
   create_table "events", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2022_03_24_151827) do
     t.datetime "date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "creator_id"
+    t.index ["creator_id"], name: "index_events_on_creator_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -28,8 +30,11 @@ ActiveRecord::Schema.define(version: 2022_03_24_151827) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "events", "users", column: "creator_id"
 end
