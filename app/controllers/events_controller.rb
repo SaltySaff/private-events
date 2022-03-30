81aa28f822d89dc5749cee1a4325ea13b509987a
SaltySaff <danielalexander.ash@gmail.com>
2022-03-30 17:03:33 +0100

    Add form for inviting users

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -22,6 +22,7 @@ class EventsController < ApplicationController
   def show
     @event = Event.find(params[:id])
     @events = Event.all
+    @users = User.all
     @invitation = Invitation.new
   end
 
--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -6,8 +6,12 @@ class InvitationsController < ApplicationController
   def create
     @event = Event.find(invitation_params[:event_id])
     @invitee = User.find_by_username(invitation_params[:username])
-    @event.invitees << @invitee
-    flash[:notice] = "Invited #{@invitee.username}"
+    if @event.invitees.include?(@invitee) || @invitee == @event.creator
+      flash[:alert] = "User has already been invited."
+    else
+      @event.invitees << @invitee
+      flash[:notice] = "Invited #{@invitee.username}"
+    end
     redirect_to root_path
   end
 
